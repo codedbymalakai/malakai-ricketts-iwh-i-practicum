695544e3232634adf0fd90c476d8ac65554e1e28
Malakai Ricketts <codedbymalakai@gmail.com>
2025-12-15 16:56:21 +0000

    First commit to my Integrating With HubSpot 1: Foundations practicum repository

--- index.js
+++ index.js
@@ -1,14 +1,87 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const axios = require("axios");
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HUBSPOT_HEADERS = {
+  Authorization: `Bearer ${process.env.HUBSPOT_ACCESS_TOKEN}`,
+  "Content-Type": "application/json"
+};
+
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static("public"));
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT_TYPE}?properties=name,species,watering_frequency`,
+      { headers: HUBSPOT_HEADERS }
+    );
+
+    res.render("homepage", {
+      title: "Homepage | Integrating With HubSpot I Practicum",
+      records: response.data.results
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send("Error fetching records");
+  }
+});
+
+
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum"
+  });
+});
+
+app.post("/update-cobj", async (req, res) => {
+  try {
+    await axios.post(
+      `https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT_TYPE}`,
+      {
+        properties: {
+          name: req.body.name,
+          species: req.body.species,
+          watering_frequency: req.body.watering_frequency
+        }
+      },
+      { headers: HUBSPOT_HEADERS }
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send("Error creating record");
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
+// const express = require('express');
+// const axios = require('axios');
+// const app = express();
+
+// app.set('view engine', 'pug');
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -68,4 +141,4 @@ app.post('/update', async (req, res) => {
 
 
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+// app.listen(3000, () => console.log('Listening on http://localhost:3000'));
